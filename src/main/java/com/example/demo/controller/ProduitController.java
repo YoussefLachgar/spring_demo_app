30827a89987494f4a14788f872546b4bd29d0d12
Lachgar Youssef <lachgar.youssef0@gmail.com>
2023-05-21 20:23:33 +0100

    1: j'ai ajouter thymeleaf au projet et je testee el fonction bien

--- src/main/java/com/example/demo/controller/ProduitController.java
+++ src/main/java/com/example/demo/controller/ProduitController.java
@@ -1,9 +1,13 @@
 package com.example.demo.controller;
 
 import com.example.demo.dao.ProduitRepository;
+import com.example.demo.entites.Produit;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 public class ProduitController {
@@ -11,7 +15,9 @@ public class ProduitController {
     private ProduitRepository produitRepository;
 
     @RequestMapping(value = "/index")
-    public String index(){
+    public String index(Model model){
+        List<Produit> produits = produitRepository.findAll();
+        model.addAttribute("listProduits",produits);
         return "produits";
     }
 }
